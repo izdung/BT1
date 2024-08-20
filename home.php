5aa8743432f53651861c866dd601275772de48c9
izdung <trngocdung04@gmail.com>
2024-08-20 18:29:40 +0700

    bt1

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo '<h1 style="color: blue">hello world</h1>'
+?>
+
+
+<?php
+echo '<button style="color: green">Click Here</button>';
+echo '<a href="https://www.google.com/"> Website </a>';
+?>
+
+
+
+</body>
+</html>
